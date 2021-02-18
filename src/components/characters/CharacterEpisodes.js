bcfbbbdbb7f9a76aa7e7b9d59abe8802635723a5
Kyle Collins <kyle.collins7737@gmail.com>
2021-02-17 21:20:53 -0500

    fixed minor error in CharacterEpisodes.js

--- src/components/characters/CharacterEpisodes.js
+++ src/components/characters/CharacterEpisodes.js
@@ -1,5 +1,28 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+const CharacterEpisodes = ({ items = { results: [] } }) => {
+  const { id } = useParams();
+  const episodes =
+    items?.results && items.results.filter((el) => el.id.toString() === id)[0];
+  const episodeList = episodes?.episode
+    ? episodes.episode.map((ep, i) => (
+        <li key={i}>
+          <a href={ep}>{ep}</a>
+        </li>
+      ))
+    : [];
+  return (
+    <div>
+      <ul>
+        {episodeList.length > 0 ? episodeList : <li>Loading Episodes...</li>}
+      </ul>
+    </div>
+  );
+};
+export default CharacterEpisodes;
+
+/*import React from "react";
+import { useParams } from "react-router-dom";
 
 const CharacterEpisodes = ({ items = { results: [] } }) => {
   const { id } = useParams();
@@ -23,3 +46,4 @@ const CharacterEpisodes = ({ items = { results: [] } }) => {
 };
 
 export default CharacterEpisodes;
+*/
